43ae7832a74acd65c977985a344779c2a6077882
Mohara88s <73835861+Mohara88s@users.noreply.github.com>
2023-08-28 14:02:24 +0300

    server moved to onreder.com

--- src/redux/auth/auth-operations.js
+++ src/redux/auth/auth-operations.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 // axios.defaults.baseURL = 'http://127.0.0.1:3000/api';
-axios.defaults.baseURL = 'https://cll-server-production.up.railway.app/api';
+axios.defaults.baseURL = 'https://cll-server.onrender.com/api';
 
 const token = {
   set(token) {
